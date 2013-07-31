5346396d0906431ca9395a3938c70e5433b4eeca
cocolekat <cocolekat@gmail.com>
2013-07-31 17:43:29 -0400

    Refactored id to module standard: moduleNameModule-variable
    Signed-off-by: cocolekat <cocolekat@gmail.com>

--- dojo/WebContent/modules/logopane.js
+++ dojo/WebContent/modules/logopane.js
@@ -3,11 +3,12 @@ define(["dijit/layout/ContentPane"], function(ContentPane) {
     	getLogoPane: function() {	
 			// Logo Pane
 	        var logoPane = new ContentPane({
-				id:"logopane",
+				id:"logoPaneModule-logoPane",
 				style:"width:200px; padding:0px; margin-left:10px;",
 				content:'<img src =\"../images/logo.jpg\" width = \"200px\" height = \"95px\" alt = \"Google\"/>'
-			}, "logopane");	
-	        return logoPane;	            
+			}, "logoPane"); 
+	        // Do not use the startup() method, this will break the layout.
+	        // There is not need to return the var since it is map to and HTML id
         }
     };
 });
