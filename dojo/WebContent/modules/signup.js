ea24533eddfe8737f5c1e9d1ce960a1180af26dc
cocolekat <cocolekat@gmail.com>
2013-08-01 13:47:41 -0400

    Added email retype validation.
    Signed-off-by: cocolekat <cocolekat@gmail.com>

--- dojo/WebContent/modules/signup.js
+++ dojo/WebContent/modules/signup.js
@@ -13,7 +13,17 @@ define(["dijit/form/Form", "dojo/dom", "dojo/dom-construct", "dijit/layout/Conte
 			
 			signupForm.on("submit", function() {			
 				if(this.validate()) {
-					return true;
+					var formValues = this.getValues();
+					var newEmail = formValues["signupModule-signUpEmail"];
+					var vrfEmail = formValues["signupModule-retypeSignUpEmail"];
+					
+					if(newEmail != vrfEmail) {
+						alert("Your email does not match!");
+						return false;
+					}
+					else {
+						return true;
+					}
 				}
 				else {
 					return false;
@@ -28,8 +38,8 @@ define(["dijit/form/Form", "dojo/dom", "dojo/dom-construct", "dijit/layout/Conte
         	
         	var signUpMsgPane = new ContentPane({
 				id:"signupModule-signupMsgPane",	           
-	            style:"width:425px; margin:0px 0px 0px 5px;; padding:0px 0px 10px 0px",
-	            content:"<h1>IDM Sign Up</h1><p>This will keep your Dashboard and Portal account in sync</p>"
+	            style:"width:425px; margin:0px 0px 0px 5px;; padding:0px 0px 0px 0px",
+	            content:"<h1>Sign Up</h1><p>This will keep your Dashboard and Portal account in sync</p>"
 			});
         	
         	var signUpFieldsPane = new ContentPane({
@@ -47,8 +57,7 @@ define(["dijit/form/Form", "dojo/dom", "dojo/dom-construct", "dijit/layout/Conte
             	trim:true,
             	//lowercase:true,
             	//regExp:"[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,4}",
-            	placeHolder:"First Name",
-            	promptMessage:"First Name",
+            	placeHolder:"First Name",           	
             	missingMessage:"First Name is required",
             	style:"width:175px; margin:0px 10px 0px 5px; padding:0px",
             	tooltipPosition:['below']});    		
@@ -64,7 +73,6 @@ define(["dijit/form/Form", "dojo/dom", "dojo/dom-construct", "dijit/layout/Conte
             	//lowercase:true,
             	//regExp:"[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,4}",
             	placeHolder:"Last Name",
-            	promptMessage:"Last Name",
             	missingMessage:"Last Name is required",
             	style:"width:175px; margin:0px; padding:0px",
             	tooltipPosition:['below']});  
@@ -79,9 +87,25 @@ define(["dijit/form/Form", "dojo/dom", "dojo/dom-construct", "dijit/layout/Conte
 	        	trim:true,
 	        	lowercase:true,
 	        	regExp:"[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,4}",
-	        	placeHolder:"Email",
+	        	placeHolder:"Your Email",
 	        	missingMessage:"Email is required",
 	        	style:"width:362px; margin:0px 0px 0px 5px; padding:0px",
+	        	invalidMessage:"Incorrect email format",
+	        	tooltipPosition:['below']});
+			
+			// Retype Email
+			var retypesignupemail = new ValidationTextBox({
+				id:"signupModule-retypeSignUpEmail",
+				name:"signupModule-retypeSignUpEmail",
+				type:"text", 
+				required:true,
+				maxLength:"50",
+	        	trim:true,
+	        	lowercase:true,
+	        	regExp:"[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,4}",
+	        	placeHolder:"Re-enter Email",
+	        	missingMessage:"Email is required",
+	        	style:"width:362px; margin:0px 0px 15px 5px; padding:0px",
 	        	invalidMessage:"Incorrect email format",
 	        	tooltipPosition:['below']});
 			
@@ -97,19 +121,27 @@ define(["dijit/form/Form", "dojo/dom", "dojo/dom-construct", "dijit/layout/Conte
 			signUpFieldsPane.addChild(firstname);
 			signUpFieldsPane.addChild(lastname);
 			signUpFieldsPane.addChild(signupemail);
+			signUpFieldsPane.addChild(retypesignupemail);
 			signUpFieldsPane.addChild(signupButton);
 					
     		// Add the widgets to the main pane	
     		signupPane.addChild(signUpMsgPane);
     		signupPane.addChild(signUpFieldsPane);
-    		    		
+    		  
+    		// Break for last name
     		var lastNameNode = dom.byId("widget_signupModule-lastname");
     		var lastNameBreakNode = domConstruct.toDom("<br><br>");
 			domConstruct.place(lastNameBreakNode, lastNameNode, "after");
 			
+			// Break for email
 			var emailNode  = dom.byId("widget_signupModule-signUpEmail");
 			var emailBreakNode = domConstruct.toDom("<br><br>");
 			domConstruct.place(emailBreakNode, emailNode, "after");
+			
+			// Break for retype email
+			var retypeEmailNode  = dom.byId("widget_signupModule-retypeSignUpEmail");
+			var retypeEmailBreakNode = domConstruct.toDom("<br>");
+			domConstruct.place(retypeEmailBreakNode, retypeEmailNode, "after");
 					       	
         }
     };
