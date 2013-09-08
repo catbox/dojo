56b00332c1c9e480e06202cfa4041b00ba5fc47d
cocolekat <cocolekat@gmail.com>
2013-09-07 20:15:40 -0400

    Login form with response in ajax manner.
    Signed-off-by: cocolekat <cocolekat@gmail.com>

---
+++ dojo/WebContent/form/formValidator/dog.jsp
@@ -0,0 +1,18 @@
+<%
+String email = request.getParameter("email").trim();
+String password = request.getParameter("password").trim();
+
+if(email.equals("ged@go.com") && password.equals("123")) {
+	// Set the user
+	session.setAttribute("user", email);
+	response.sendRedirect("welcome.jsp");
+}
+if(!email.equals("ged@go.com")) {
+	session.setAttribute("login-error", "err-401");
+	out.println("err-401");
+}
+else if(!password.equals("123")) {
+	session.setAttribute("login-error", "err-402");
+	out.println("err-402");
+}
+%>
---
+++ dojo/WebContent/modules/loginPaneExtForm.js
@@ -0,0 +1,109 @@
+define(["dojo/dom", "dijit/form/Form", "dojo/request", "dojo/dom-form", "dijit/layout/ContentPane", "dijit/form/ValidationTextBox", 
+         "dijit/form/Button", "dojox/validate/web"], 
+         function(dom, Form, request, domForm, ContentPane, ValidationTextBox, Button) {
+
+    return {
+    	getLoginPane: function() {
+    		
+    		// Login Pane
+    		var loginPane = new ContentPane({
+				id:"loginModule-loginPane"
+			},"loginPane");
+    		
+    		// Login Field
+    		var loginFields = new ContentPane({
+				id:"loginModule-loginFields",	           
+	            style:"width:430px; max-width:430px; margin:0px; padding:5px; border-style:solid; border-width:1px; border-color:#AAAAAA; float:right;"
+			});
+    		
+			// Email Field
+			var email = new ValidationTextBox({
+				id:"loginModule-emailField",
+				name:"email",
+				type:"text", 
+				required:true,
+				maxLength:"50",
+	        	trim:true,
+	        	lowercase:true,
+	        	regExp:"[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,4}",
+	        	placeHolder:"Email",
+	        	missingMessage:"Email is required",
+	        	invalidMessage:"Incorrect email format",
+	        	style:"width:175px; margin-right:5px; padding:0px",
+	        	tooltipPosition:['below']});
+			loginFields.addChild(email);
+		    
+		    // Password Field
+			var password = new ValidationTextBox({
+				id:"loginModule-passwordField",
+				name:"password",
+				type:"password", 
+				required:true,
+				maxLength:"50",
+	        	trim:true,
+	        	lowercase:true,
+	        	//regExp:"[a-z0-9._%+-]",
+	        	placeHolder:"Password",
+	        	missingMessage:"Password is required",
+	        	invalidMessage:"Incorrect password format",
+	        	style:"width:175px; margin-right:5px; padding:0px",
+	        	tooltipPosition:['below']});
+			loginFields.addChild(password);
+		    
+	        // Submit Button
+	        var mySubmit = new Button({
+	        	id:"loginModule-loginButton",
+	            label:"Login",
+	            type:"submit"
+	        });
+	        loginFields.addChild(mySubmit);
+	        
+	        // Add the login fields to the login pane
+	        loginPane.addChild(loginFields);
+	                       
+	        // The form
+			var loginForm = new Form({
+				id:"loginModule-loginForm",
+				//method:"post",
+				//action:"../form/formValidator/authenticator.jsp"
+				method:"post",
+				action:""
+				}, "loginForm");
+			
+			loginForm.on("submit", function(evt) {			
+				if(loginForm.validate()) {
+
+					var eml = domForm.fieldToObject(dom.byId("loginModule-loginForm").email);
+					var pwd = domForm.fieldToObject(dom.byId("loginModule-loginForm").password);
+					var lang = "ES";
+					
+					
+					// prevent the page from navigating after submit
+					evt.stopPropagation();
+					evt.preventDefault();
+					
+					// Post the data to the server
+					request.post("../form/formValidator/dog.jsp", {
+						// Send the username, password and language
+						data: {
+				            email:eml,
+				            password:pwd,
+				            language:lang
+				        },
+				        
+						// Time out after 5 seconds
+						timeout: 5000
+					}).then(function(response){
+						dom.byId('errormsg').innerHTML = response;
+					});
+					
+					
+					return true;
+				}
+				else {
+					return false;
+				}			
+			});
+        }
+    };
+});
